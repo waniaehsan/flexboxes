019469805c746bc5279fad6c6b9b56c4ccd692b9
hashirahsan <ehsanareva@gmail.com>
2024-11-08 11:07:57 +0500

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
 export default function Home() {
-  return (<div className="flex flex-row gap-10 justify-center items-center min-h-screen">
-    <div className="flexbox bg-red-500 h-80 w-80"></div>
-      <div className="flexbox bg-blue-500 h-80 w-80"></div>
-      <div className="flexbox bg-green-500 h-80 w-80"></div>
-      <div className="flexbox bg-white h-80 w-80"></div>  
+  return (
+  <div>
+  <div className="flex flex-row gap-20 justify-center items-center min-h-screen">
+    <div className="flexbox bg-blue-500 h-60 w-60"></div>
+      <div className="flexbox bg-yellow-500 h-60 w-60"></div>
+      <div className="flexbox bg-green-500 h-60 w-60"></div>
+       
+    </div>
+<div className="flex flex-row gap-20 justify-center items-center ">
+<div className="flexbox bg-blue-500 h-60 w-60"></div>
+<div className="flexbox bg-yellow-500 h-60 w-60"></div>
+
+</div >
+
+
     </div>
   );
 }
